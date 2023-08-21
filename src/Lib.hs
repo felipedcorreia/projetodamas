5bfbe4906630dec020736d8d702ca4de449b65b7
gabrielamonaro <gabriela.monaro@aluno.ufabc.edu.brgit config --global user.email gabriela.monaro@aluno.ufabc.edu.br>
2023-08-21 20:48:29 -0300

    feat: board-coordinates

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,19 +8,26 @@ type Board = [[Cell]]
 
 emptyCell :: Int -> Cell
 emptyCell n
-    | even n = ("  ", "\x1b[41m")  -- Célula com fundo vermelho
-    | otherwise = ("  ", "\x1b[44m")  -- Célula com fundo azul
+    | even n = ("   ", "\x1b[41m")  -- Célula com fundo vermelho
+    | otherwise = ("   ", "\x1b[44m")  -- Célula com fundo azul
+
 
 emptyBoard :: Int -> Board
 emptyBoard size = [[emptyCell (x + y) | x <- [0..size-1]] | y <- [0..size-1]]
+
 
 showCell :: Cell -> String
 showCell (content, color) = color ++ content ++ "\x1b[0m"  -- Reset da cor
 
 showBoard :: Board -> IO ()
-showBoard board = putStrLn $ unlines $ map showRow board
+showBoard board = do
+    let letters = "87654321"
+    let numberedRows = zip [1..] board
+    putStr (unlines (map (showRow letters) numberedRows))
+    putStrLn "   A  B  C  D  E  F  G  H"
   where
-    showRow row = concatMap (\cell -> showCell cell ++ "") row
+    showRow letters (rowNum, row) = letters !! (rowNum - 1) : " " ++ concatMap (\cell -> showCell cell ++ "") row
+    
 
 someFunc :: IO ()
 someFunc = showBoard (emptyBoard 8)
