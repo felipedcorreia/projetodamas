d3e605d84239fc2eacef99e82f1829d9eec6e785
Gabriela Monaro Rodrigues <74309940+gabrielamonaro@users.noreply.github.com>
2023-08-23 01:00:41 +0000

    setting new type - Content

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,43 +2,57 @@ module Lib
     ( someFunc
     ) where
 
--- instance Show Content where
---     show Black = " ● "
---     show White = " ○ "
---     show Empty = "   "
+data Content = Black | White | Empty 
+instance Show Content where
+    show Black = " ● "
+    show White = " ○ "
+    show Empty = "   "
 
 type Coord = (Int, Int)
-type Cell = (String, String, Coord)  -- (String, Cor)
+type Cell = (Content, String, Coord)  -- (String, Cor)
 
 type Board = [[Cell]]
 
-emptyCell :: Int -> Int -> Int -> Cell
-emptyCell n x y
-    | even n =  ("   ", "\x1b[41m", (x, y))  -- Célula com fundo vermelho
+initialSetCell :: Int -> Int -> Int -> Cell
+initialSetCell n x y
+    | even n =  (Empty, "\x1b[41m", (x, y)) 
     | otherwise = if (y < 3) 
-        then (" ● ", "\x1b[44m", (x, y)) 
+        then (Black, "\x1b[44m", (x, y)) 
         else 
             if (y > 4) 
-                then (" ○ ", "\x1b[44m", (x, y))
-                else ("   ", "\x1b[44m", (x, y))  -- Célula com fundo azul
+                then (White, "\x1b[44m", (x, y))
+                else (Empty, "\x1b[44m", (x, y))  
 
+----Showing coordinates
+-- emptyCell :: Int -> Int -> Int -> Cell
+-- emptyCell n x y
+--     | even n = (" x: " ++ show x ++ " y:" ++ show y ++ " ", "\x1b[41m", (x, y))  
+--     | otherwise = (" x: " ++ show x ++ " y:" ++ show y ++ " ", "\x1b[44m", (x, y))  
+    
+-- emptyCell :: Int -> Int -> Int -> Cell
+-- emptyCell n x y
+--     | even n = ("   ", "\x1b[41m", (x, y))  -- Célula com fundo vermelho
+--     | otherwise = ("   ", "\x1b[44m", (x, y))  -- Célula com fundo azul
 
 emptyBoard :: Int -> Board
-emptyBoard size = [[emptyCell (x + y) x y | x <- [0..size-1]] | y <- [0..size-1]]
+emptyBoard size = [[initialSetCell (x + y) x y | x <- [0..size-1]] | y <- [0..size-1]]
 
 
 showCell :: Cell -> String
-showCell (content, color, coord) = color ++ content ++ "\x1b[0m"  -- Reset da cor
+showCell (content, color, coord) = color ++ show content ++ "\x1b[0m"  -- Reset da cor
 
 showBoard :: Board -> IO ()
 showBoard board = do
-    let letters = "87654321"
+    let letters = "12345678"
     let numberedRows = zip [1..] board
-    putStr (unlines (map (showRow letters) numberedRows))
     putStrLn "   A  B  C  D  E  F  G  H"
+    putStr (unlines (map (showRow letters) numberedRows))
+    
   where
     showRow letters (rowNum, row) = letters !! (rowNum - 1) : " " ++ concatMap (\cell -> showCell cell ++ "") row
-    
+
+-- movePiece :: Board -> Coord -> Coord -> Board
+-- movePiece (x, y) (x,y) (x', y') = 
 
 someFunc :: IO ()
 someFunc = showBoard (emptyBoard 8)
