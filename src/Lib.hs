79c8eeb09c6936d085bd67981ea0afff97255856
Gabriela Monaro Rodrigues <74309940+gabrielamonaro@users.noreply.github.com>
2023-08-24 16:05:11 +0000

    get Element Function

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,7 @@ module Lib
     ( someFunc
     ) where
 
-data Content = Black | White | Empty 
+data Content = Black | White | Empty deriving(Eq)
 instance Show Content where
     show Black = " ● "
     show White = " ○ "
@@ -23,19 +23,22 @@ initialSetCell n x y
                 then (White, "\x1b[44m", (x, y))
                 else (Empty, "\x1b[44m", (x, y))  
 
-----Showing coordinates
--- emptyCell :: Int -> Int -> Int -> Cell
--- emptyCell n x y
+--Showing coordinates
+-- initialSetCell :: Int -> Int -> Int -> Cell
+-- initialSetCell n x y
 --     | even n = (" x: " ++ show x ++ " y:" ++ show y ++ " ", "\x1b[41m", (x, y))  
 --     | otherwise = (" x: " ++ show x ++ " y:" ++ show y ++ " ", "\x1b[44m", (x, y))  
-    
--- emptyCell :: Int -> Int -> Int -> Cell
--- emptyCell n x y
---     | even n = ("   ", "\x1b[41m", (x, y))  -- Célula com fundo vermelho
---     | otherwise = ("   ", "\x1b[44m", (x, y))  -- Célula com fundo azul
+
+emptyCell :: Int -> Int -> Int -> Cell
+emptyCell n x y
+    | even n = (Empty, "\x1b[41m", (x, y))  -- Célula com fundo vermelho
+    | otherwise = (Empty, "\x1b[44m", (x, y))  -- Célula com fundo azul
 
 emptyBoard :: Int -> Board
-emptyBoard size = [[initialSetCell (x + y) x y | x <- [0..size-1]] | y <- [0..size-1]]
+emptyBoard size = [[emptyCell (x + y) x y | x <- [0..size-1]] | y <- [0..size-1]]
+
+createBoard :: Int -> Board
+createBoard size = [[initialSetCell (x + y) x y | x <- [0..size-1]] | y <- [0..size-1]]
 
 
 showCell :: Cell -> String
@@ -47,12 +50,32 @@ showBoard board = do
     let numberedRows = zip [1..] board
     putStrLn "   A  B  C  D  E  F  G  H"
     putStr (unlines (map (showRow letters) numberedRows))
-    
+
   where
     showRow letters (rowNum, row) = letters !! (rowNum - 1) : " " ++ concatMap (\cell -> showCell cell ++ "") row
 
+showContent :: Content -> String
+showContent n = show n
+
 -- movePiece :: Board -> Coord -> Coord -> Board
--- movePiece (x, y) (x,y) (x', y') = 
+-- movePiece board (x,y) (x', y') = do
+--     -- (board !! row) !! col
+
+
+getCell :: Board -> Int -> Int -> Cell
+getCell board row col = (board !! row) !! col
+
+getContent :: Cell -> Content
+getContent (x, _, _) = x
+
+verifyContent :: Content -> Bool
+verifyContent content = content == White
 
 someFunc :: IO ()
-someFunc = showBoard (emptyBoard 8)
+someFunc = do
+    let newBoard = (emptyBoard 8)
+    let cell = getCell newBoard 0 0
+    let content = getContent (getCell newBoard 0 0)
+    showBoard (newBoard)
+    putStrLn (showCell cell)
+    putStrLn (showContent content)    
